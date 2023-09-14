a4e20de2539ed625d8053b125d25074b80833fa4
mertdmkrn37 <mertdmkrn37@gmail.com>
2023-09-14 17:46:34 +0300

    Düzeltme.

--- CareGardenApiV1/Model/RequestModel/BusinessWorkInfoModel.cs
+++ CareGardenApiV1/Model/RequestModel/BusinessWorkInfoModel.cs
@@ -8,7 +8,7 @@ namespace CareGardenApiV1.Model.RequestModel
         public Guid businessId { get; set; } = Guid.Empty;
 
         public BusinessWorkingInfo businessWorkingInfo { get; set; } = new BusinessWorkingInfo();
-        public bool officialDayAvailable { get; set; }
+        public bool officialHolidayAvailable { get; set; }
         public int appointmentTimeInterval { get; set; }
         public int appointmentPeopleCount { get; set; }
         public WorkingGenderType workingGenderType { get; set; }

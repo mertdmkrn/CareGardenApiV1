b3e6f81c88a8dd2f0d8dbc2ddf19ba132b12bd66
pc <pc@DESKTOP-86M3HVC>
2024-09-08 13:44:52 +0300

    Düzeltme.

--- CareGardenApiV1/Helpers/Constants.cs
+++ CareGardenApiV1/Helpers/Constants.cs
@@ -4,7 +4,7 @@ namespace CareGardenApiV1.Helpers
 {
     public static class Constants
     {
-        public static bool IsTurkish = Resource.Resource.Culture.ToString().Equals("tr");
+        public static bool IsTurkish = Resource.Resource.Culture.ToString().Contains("tr");
         public static string BusinessAdminPanelUrl = "https://mertdmkrn.github.io/cg-business/#/";
 
         public static class CacheKeys
